3046d46d1093ab766ef9459a02dcc6fd17f2ff9a
codercodemanish <72806831+codercodemanish@users.noreply.github.com>
2021-12-10 10:39:58 +0530

    check

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     <!-- Navination start -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Eco-Electron</a>
+            <a class="navbar-brand" href="#"> Eco-Electron </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"            data-bs-target="#navbarSupportedContent"aria-controls="navbarSupportedContent" aria-expanded="false"           aria-label="Togglenavigation">
             <span class="navbar-toggler-icon"></span>
             </button>
